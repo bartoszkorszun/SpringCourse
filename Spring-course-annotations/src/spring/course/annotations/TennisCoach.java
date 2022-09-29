98a5120a23cfc94e1424c2e0579b44267469b01a
Bartosz Korszun <bartoszkorszun@gmail.com>
2022-09-29 14:02:51 +0200

    Learning default component names

--- Spring-course-annotations/src/spring/course/annotations/TennisCoach.java
+++ Spring-course-annotations/src/spring/course/annotations/TennisCoach.java
@@ -2,7 +2,7 @@ package spring.course.annotations;
 
 import org.springframework.stereotype.Component;
 
-@Component("theTennisCoach")
+@Component
 public class TennisCoach implements Coach {
 
 	@Override
