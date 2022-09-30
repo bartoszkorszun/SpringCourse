dfe9387bb3225bf2ef0bf8a1702c236ceb152976
Bartosz Korszun <bartoszkorszun@gmail.com>
2022-09-30 15:29:01 +0200

    Learning Scope Annotations

---
+++ Spring-course-annotations/src/spring/course/annotations/AnnotationBeanScopeDemoApp.java
@@ -0,0 +1,31 @@
+package spring.course.annotations;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		
+		// LOAD SPRING CONFIG FILE
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// RETRIEVE BEAN FROM THE SPRING CONTAINER
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		
+		Coach alphaCoach = context.getBean("tennisCoach", Coach.class);
+		
+		// CHECK IF THEY ARE THE SAME
+		boolean result = (theCoach == alphaCoach);
+		
+		// PRINTING THE RESULT 
+		System.out.println("\n>>Pointing to the same object: " + result);
+		
+		System.out.println("\n>>Memoty location of theCoach: " + theCoach);
+		
+		System.out.println("\n>>Memoty location of alphaCoach: " + alphaCoach);
+		
+		// CLOSE THE CONTEXT
+		context.close();
+	}
+
+}
